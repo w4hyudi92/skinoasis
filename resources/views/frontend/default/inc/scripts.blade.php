96950bcb86b88030fcf4f77db6ff6417cfc6d7c1
NuryantoWahyudi <nuryantowahyudi8@gmail.com>
2024-01-12 08:55:43 +0700

    Tawkto

--- resources/views/frontend/default/inc/scripts.blade.php
+++ resources/views/frontend/default/inc/scripts.blade.php
@@ -15,6 +15,19 @@
 <script src="{{ staticAsset('frontend/common/js/toastr.min.js') }}"></script>
 <script src="{{ staticAsset('frontend/common/js/select2.js') }}"></script>
 <script src="{{ staticAsset('frontend/default/assets/js/app.js') }}"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/65a09b838d261e1b5f5225bd/1hjtluuil';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 
 <script>
     "use strict"
