17e3ddcdc4b2ea8d60b122e7f32a4c538e22cb97
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-01-31 09:43:53 +0700

    Update home.blade.php

--- resources/views/frontend/skinoasis/pages/home.blade.php
+++ resources/views/frontend/skinoasis/pages/home.blade.php
@@ -260,14 +260,14 @@
             </div><!-- End .owl-carousel -->
 
             <!-- Flyer Home 1 -->
-            <section class="gallery pt-5" style="background-image: url(assets/images/demos/demo-30/bg-3.png)">
+            <section class="gallery pt-5" style="background-image: url({{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/bg-3.png') }})">
                 <div class="container">
                     <div class="row">
 
                         <div class="col-md-6">
                             <div class="content-right">
                                 <figure class="mb-md-12">
-                                    <img src="assets/images/demos/demo-30/banners/banner-2.jpg" alt="Banner">
+                                    <img src="{{ staticAsset('frontend/skinoasis/assets/images/demos/demo-30/banners/banner-2.jpg') }}" alt="Banner">
                                 </figure>
                             </div>
                         </div>
@@ -279,7 +279,7 @@
                                     <p class="scan-here mt-4 mb-2">
                                         Scan Here
                                     </p>
-                                    <img class="barcode" src="assets/images/barcode.png" alt="Banner" width="50" height="20">
+                                    <img class="barcode" src="{{ staticAsset('frontend/skinoasis/assets/images/barcode.png') }}" alt="Banner" width="50" height="20">
 
                                 </div>
                             </div>
