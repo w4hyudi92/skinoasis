7c19237c77d396f317816825dd2e1a94b8337021
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-01-31 09:40:21 +0700

    Update home.blade.php

--- resources/views/frontend/skinoasis/pages/home.blade.php
+++ resources/views/frontend/skinoasis/pages/home.blade.php
@@ -433,29 +433,22 @@
 
             <div class="footer-bottom mb-5">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
+                    <p class="footer-copyright">Copyright © {{ date('Y') }} Skinoasis. All Rights Reserved.</p><!-- End .footer-copyright -->
+                    <ul class="footer-menu">
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul><!-- End .footer-menu -->
 
-                        </div>
-                        <div class="col-md-4">
-                            <p class="footer-copyright">Copyright © 2020 Skinoasis Store. All Rights Reserved.</p><!-- End .footer-copyright -->
-                        </div>
-                        <div class="col-md-4">
-                            
-                            <figure class="footer-payments">
-                                <div class="social-icons">
-                                    <a href="#" class="social-icon" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
-                                    <a href="#" class="social-icon" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
-                                    <a href="#" class="social-icon" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
-                                    <a href="#" class="social-icon" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
-                                    <a href="#" class="social-icon" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>
-                                </div><!-- End .soial-icons -->
-                            </figure><!-- End .footer-payments -->
-                        </div>
-                    </div>
-                </div>
+                    <div class="social-icons social-icons-color">
+                        <span class="social-label">Social Media</span>
+                        <a href="#" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
+                        <a href="#" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                        <a href="#" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
+                        <a href="#" class="social-icon social-youtube" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
+                        <a href="#" class="social-icon social-pinterest" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>
+                    </div><!-- End .soial-icons -->
+                </div><!-- End .container -->
             </div><!-- End .footer-bottom -->
-
         </footer><!-- End .footer -->
     </div><!-- End .page-wrapper -->
     <button id="scroll-top" title="Back to Top"><i class="icon-arrow-up"></i></button>
